a92f3c83bf709bffd8de8093d10a4fa28fb8607d
Charlie Sinder <cmsinder@umich.edu>
2019-12-08 17:16:29 -0500

    Takes IMDb Id's from OMDB and uses BeautifulSoup from Box Office Mojo to recieve box office profit.

--- Final-Project.py
+++ Final-Project.py
@@ -1,18 +1,18 @@
 import json
 import requests
-
+from bs4 import BeautifulSoup
 
 ###   TMDB Gives us a bunch of popular movies   ###
 
-TMDB_url = "https://api.themoviedb.org/3/movie/popular?api_key=773f87a98c4a4962613a1c61319b7edd&language=en-US&"
-TMDB_params = {'page':'1'}
+tmdb_url = "https://api.themoviedb.org/3/movie/popular?api_key=773f87a98c4a4962613a1c61319b7edd&language=en-US&"
+tmdb_params = {'page':'1'}
 
-TMDB_r = requests.get(url = TMDB_url, params = TMDB_params)
-TMDB_data = TMDB_r.json()
+tmdb_r = requests.get(url = tmdb_url, params = tmdb_params)
+tmdb_data = tmdb_r.json()
 
 l_of_movs = []
 
-for movie in TMDB_data['results']:
+for movie in tmdb_data['results']:
     m_title = movie['title']
     l_of_movs.append(m_title)
 
@@ -20,20 +20,34 @@ for movie in TMDB_data['results']:
 
 
 
-###   OMDB Takes list of movies and returns tuple of (Title, Score, Box Office) ###
+###   OMDB Takes list of movies and returns tuple of (Title, Score) ###
 
-OMDB_url = "http://www.omdbapi.com/?apikey=357b9dcf&"
+omdb_url = "http://www.omdbapi.com/?apikey=357b9dcf&"
 
 for mov in l_of_movs:
-    OMDB_params = {'t':mov}
+    omdb_params = {'t':mov}
 
-    OMDB_r = requests.get(url = OMDB_url, params = OMDB_params)
-    OMDB_data = OMDB_r.json()
+    omdb_r = requests.get(url = omdb_url, params = omdb_params)
+    omdb_data = omdb_r.json()
     
-    mov_name = OMDB_data['Title']
-    for rate in OMDB_data['Ratings']:
+    mov_name = omdb_data['Title']
+    for rate in omdb_data['Ratings']:
         if rate['Source'] == 'Rotten Tomatoes':
             mov_score = rate['Value']
-    mov_box = OMDB_data['BoxOffice']
+    mov_imdbid = omdb_data['imdbID']
+
+
+###   Take mov_imdbid and put into Box Office Mojo    ###
+
+    bom_url = "https://www.boxofficemojo.com/title/{}/?ref_=bo_se_r_1".format(mov_imdbid)
+    soup = BeautifulSoup(requests.get(bom_url).text, 'html.parser')
+    sum_tab = soup.find('div', class_ = "a-section a-spacing-none mojo-performance-summary-table")
+    sum_tab_divs = sum_tab.find_all('div', class_ = "a-section a-spacing-none")
+    for divs in sum_tab_divs:
+        if 'Worldwide' in divs.find('span', class_ = "a-size-small").text:
+            mov_box = divs.find('span', class_ = "money").text
+
+
+
     mov_tup = (mov_name, mov_score, mov_box)
     print(mov_tup)
