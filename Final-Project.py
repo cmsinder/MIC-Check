5c01c951591418d044aba62ad0cde65f5f6b609f
Charlie Sinder <cmsinder@umich.edu>
2019-12-08 16:28:25 -0500

    Works, but not if OMDB doesn't have the movie.

--- Final-Project.py
+++ Final-Project.py
@@ -4,37 +4,36 @@ import requests
 
 ###   TMDB Gives us a bunch of popular movies   ###
 
-url = "https://api.themoviedb.org/3/movie/popular?api_key=773f87a98c4a4962613a1c61319b7edd&language=en-US&"
-params = {'page':'1'}
+TMDB_url = "https://api.themoviedb.org/3/movie/popular?api_key=773f87a98c4a4962613a1c61319b7edd&language=en-US&"
+TMDB_params = {'page':'1'}
 
-r = requests.get(url = url, params = params)
-data = r.json()
+TMDB_r = requests.get(url = TMDB_url, params = TMDB_params)
+TMDB_data = TMDB_r.json()
 
 l_of_movs = []
 
-for movie in data['results']:
+for movie in TMDB_data['results']:
     m_title = movie['title']
     l_of_movs.append(m_title)
 
-print(l_of_movs)
-print(len(l_of_movs))
+
 
 
 
 ###   OMDB Takes list of movies and returns tuple of (Title, Score, Box Office) ###
 
-url = "http://www.omdbapi.com/?apikey=357b9dcf&"
+OMDB_url = "http://www.omdbapi.com/?apikey=357b9dcf&"
 
-for mov in mov_list:
-    params = {'t':mov}
+for mov in l_of_movs:
+    OMDB_params = {'t':mov}
 
-    r = requests.get(url = url, params = params)
-    data = r.json()
+    OMDB_r = requests.get(url = OMDB_url, params = OMDB_params)
+    OMDB_data = OMDB_r.json()
     
-    mov_name = data['Title']
-    for rate in data['Ratings']:
+    mov_name = OMDB_data['Title']
+    for rate in OMDB_data['Ratings']:
         if rate['Source'] == 'Rotten Tomatoes':
             mov_score = rate['Value']
-    mov_box = data['BoxOffice']
+    mov_box = OMDB_data['BoxOffice']
     mov_tup = (mov_name, mov_score, mov_box)
     print(mov_tup)
