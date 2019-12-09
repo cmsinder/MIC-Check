366ff16e9d4c936b489639a30bb48f0df88cc46d
Charlie Sinder <cmsinder@umich.edu>
2019-12-09 18:42:24 -0500

    Creates a database and a cursor.

--- Final-Project.py
+++ Final-Project.py
@@ -1,7 +1,13 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+import os
 
+
+path = os.path.dirname(os.path.abspath(__file__))
+conn = sqlite3.connect(path+'/'+'Movies Data Base.db')
+cur = conn.cursor()
 
 
 l_of_movie_tups = []
