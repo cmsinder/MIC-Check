b74b52da62ca1399c4767c1cd38424f72ec89804
Charlie Sinder <cmsinder@umich.edu>
2019-12-11 14:09:27 -0500

    Wrote function for analyzing two tables. Returns a dictionary.

--- DBAnalysis.py
+++ DBAnalysis.py
@@ -16,9 +16,9 @@ cur.execute("SELECT rating, boxoffice FROM TitleAndRatings LEFT JOIN TitleAndBox
 dict_of_ratings = {'G': [], 'PG': [], 'PG-13': [], 'R': []}
 
 for row in cur:
-    if row[1] == 'N/A':
+    if row[0] == 'N/A':
         continue
-    if row[0] == 'G':
+    elif row[0] == 'G':
         dict_of_ratings['G'].append(row[1])
     elif row[0] == 'PG':
         dict_of_ratings['PG'].append(row[1])
@@ -45,3 +45,56 @@ for rating in dict_of_ratings.keys():
     outfile.write("The average money earned at the Box Office for movies rated {} was: $".format(rating) + str(round(statistics.mean(dict_of_ratings[rating]))) + '\n')
 
 outfile.close()
+
+
+###   Defining a function that should make calculations easy  ###
+
+
+def calculate_from_db(x, y):
+    dict_for_analysis = {}
+
+    if x == 'year':     # Set up a way to get each table. Repeat with y.
+        x_table_title = 'TitleAndReleaseYear'
+    elif x == 'genre':
+        x_table_title = 'TitleAndGenre'
+    elif x == 'score':
+        x_table_title = 'TitleAndScore'
+    elif x == 'rating':
+        x_table_title = 'TitleAndRatings'
+    elif x == 'boxoffice':
+        x_table_title = 'TitleAndBoxOffice'
+    elif x == 'holiday':
+        x_table_title = 'TitleAndHoliday'
+    elif x == 'holidayname'
+        x_table_title = 'TitleAndHolidayName'
+
+
+    if y == 'year':     # Set up a way to get each table. Repeat with y.
+        y_table_title = 'TitleAndReleaseYear'
+    elif y == 'genre':
+        y_table_title = 'TitleAndGenre'
+    elif y == 'score':
+        y_table_title = 'TitleAndScore'
+    elif y == 'rating':
+        y_table_title = 'TitleAndRatings'
+    elif y == 'boxoffice':
+        y_table_title = 'TitleAndBoxOffice'
+    elif y == 'holiday':
+        y_table_title = 'TitleAndHoliday'
+    elif y == 'holidayname'
+        y_table_title = 'TitleAndHolidayName'
+
+
+    cur.execute("SELECT {} FROM {}".format(x, x_table_title))
+    for row in cur:
+        if row not in dict_for_analysis:
+            dict_for_analysis[row] = []
+    
+    cur.execute("SELECT {}, {} FROM {} LEFT JOIN {} ON {}.title = {}.title".format(x, y, x_table_title, y_table_title, x_table_title, y_table_title))
+
+    for row in cur:
+        for k in dict_for_analysis.keys():
+            if row[0] == k:
+                dict_for_analysis[k].append(row[1])
+    
+    return dict_for_analysis
