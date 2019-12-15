c31b4cb258e4f52842cba8a64bbc25e14023c859
milanjain18 <55509711+milanjain18@users.noreply.github.com>
2019-12-15 18:31:54 -0500

    Add files via upload

--- DBAnalysis.py
+++ DBAnalysis.py
@@ -1,6 +1,10 @@
 import sqlite3
 import os
 import statistics
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+'Movies Data Base.db')
@@ -72,7 +76,7 @@ cur.execute("SELECT rating, boxoffice FROM TitleAndRatings LEFT JOIN TitleAndBox
 
 ###   MATH    ###
 
-dict_of_ratings = {'G': [], 'PG': [], 'PG-13': [], 'R': []}
+dict_of_ratings = {'G': [], 'PG': [], 'PG-13': [], 'R': [] ,'N/A': [], 'Not Rated' : [],'TV-MA' : [],'TV-PG': [] }
 
 for row in cur:
     if row[0] == 'N/A':
@@ -81,11 +85,19 @@ for row in cur:
         dict_of_ratings['G'].append(row[1])
     elif row[0] == 'PG':
         dict_of_ratings['PG'].append(row[1])
+    elif row[0] == 'TV-PG':
+        dict_of_ratings['TV-PG'].append(row[1])
     elif row[0] == 'PG-13':
         dict_of_ratings['PG-13'].append(row[1])
     elif row[0] == 'R':
         dict_of_ratings['R'].append(row[1])
-
+    elif row[0] == 'N/A':
+        dict_of_ratings['N/A'].append(row[1])
+    elif row[0] == 'Not Rated':
+        dict_of_ratings['Not Rated'].append(row[1])
+    elif row[0] == 'TV-MA':
+        dict_of_ratings['TV-MA'].append(row[1])
+ 
 
 #avg_G_bo = statistics.mean(dict_of_ratings['G'])
 #avg_PG_bo = statistics.mean(dict_of_ratings['PG'])
@@ -107,4 +119,79 @@ outfile.close()
 
 
 
-print(calculate_from_db('rating', 'boxoffice'))
+
+# print(calculate_from_db('rating', 'boxoffice'))
+
+
+
+
+#VISUAL 1 creates bar chart of the movies, sorted by genre
+rating_bo_dict = calculate_from_db('rating', 'boxoffice')
+key1 = list(rating_bo_dict.keys())
+val = []
+for i in rating_bo_dict:
+    len1 = len(rating_bo_dict[i])
+    val.append(len1)
+
+plt.bar(key1, val, color = ('green', 'blue', 'green', 'blue', 'green', 'blue', 'blue'))
+plt.xlabel("Movie Ratings")
+plt.ylabel("Quantity of Movies")
+plt.title("Ratings Fragmentation of Top 100 Movies")
+plt.tight_layout()
+# print(rating_bo_dict)
+# print(key1)
+# print(val)
+plt.show()
+
+
+
+#VISUAL 2 scatterplot of average box office each year
+year_bo_dict = calculate_from_db('year', 'boxoffice')
+#print(year_bo_dict)
+years = list(year_bo_dict.keys())
+total = year_bo_dict.values()
+avg_bo = []
+for box in total:
+    sum_bo = 0
+    num = len(box)
+    for i in box:
+        sum_bo += i
+        #print(i)
+    avg = sum_bo/num
+    avg_bo.append(avg)
+
+plt.scatter(years, avg_bo, color = 'g')
+plt.title("Gauging Average Box Office Growth Year-Over-Year")
+plt.xlabel("Year")
+plt.ylabel("Mean Box Office Revenue (in $Billions USD)")
+
+plt.show()
+
+#VISUAL 3 Scatterplot of average rating score by genre
+
+rating_score_bo = calculate_from_db('rating' , 'score')
+ratings = rating_score_bo.keys()
+scores = rating_score_bo.values()
+# print(rating_score_bo)
+
+score_a = []
+for list1 in scores:
+    sums = 0
+    len_score = len(list1)
+    if len_score > 0:
+        for i in list1:
+            sums += i
+        avg_ratingscore = sums/len_score
+        score_a.append(avg_ratingscore)
+    else:
+        score_a.append(0)
+        #need to adjust scores for N/A rating
+    
+
+plt.scatter(ratings, score_a, color = 'b')
+plt.title("Average Top Movie User Scores by Rating")
+plt.xlabel("Rating")
+plt.ylabel("Average User-Given Score")
+
+plt.show()
+
